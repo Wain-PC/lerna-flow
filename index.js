177bfef81d91ed9623f22b0c077eafb6ec984b28
Pavel Dranichnikov <psdranichnikov@avito.ru>
2019-12-18 22:45:19 +0300

    Smarter start

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
+const askChoice = require('./tools/askChoice');
 
 const run = async () => {
-    const args = yargs
-        .command('dev [type]', 'update DEV packages', (yargs) => {
-            yargs.positional('type', {
-                describe: 'one of: major, minor, patch',
-                type: 'string',
-                default: 'minor'
-            })
-        })
-        .help()
-        .argv;
+    // TODO: Use something smaller to parse args
+    const args = yargs.argv;
+    let [command] = args._;
 
-    const [command] = args._;
+    if(!command || !['dev', 'prod'].includes(command)) {
+        command = await askChoice('Select command', ['dev', 'prod'], 'dev');
+    }
+
     const runner = require('./commands/' + command + '.js');
     await runner(args, yargs);
+
+    console.log('Thanks, bye!');
 };
 
 run();
