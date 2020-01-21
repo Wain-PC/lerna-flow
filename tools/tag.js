67ed6c5913723959957c4c817a0506f37685ab5a
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-01-21 17:34:45 +0300

    Updated 'tag' command

--- tools/tag.js
+++ tools/tag.js
@@ -1,11 +1,11 @@
-const exec = require('./exec');
+const {promisify} = require('util');
+const exec = promisify(require('child_process').exec);
 const spawn = require('./spawn');
 
 module.exports = async () => {
     const now = Date.now();
     const tag = `latest_${now}`;
-    // const {stdout} = await exec('git tag -l');
-    // await exec('xargs git tag -d', {input: stdout});
+    await exec('git tag | xargs git tag -d');
     await spawn('git fetch --tags');
     await spawn(`git tag -a ${tag} -m ${tag}`);
     await spawn('git push --tags');
