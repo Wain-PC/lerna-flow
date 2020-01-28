462fd5824474a0c0453fc5ac37e4e23b96e45d23
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-01-28 15:56:12 +0300

    Removed 'version' command

--- commands/jira.js
+++ commands/jira.js
@@ -4,5 +4,5 @@ const jira = require('../tools/jira');
 module.exports = async () => {
     const jiraUrl = await jira();
     open(jiraUrl);
-
+    console.log('Jira task should now be opened in your browser');
 };
--- commands/version.js
+++
@@ -1,9 +0,0 @@
-const askType = require('../tools/askType');
-const ask = require('../tools/ask');
-const bump = require('../tools/bump');
-
-module.exports = async () => {
-    const type = await askType();
-    const push = await ask('Push updated packages to git?');
-    await bump({type, push});
-};
