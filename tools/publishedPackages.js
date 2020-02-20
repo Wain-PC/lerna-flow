ce8c75adec61f7d5c7a35b81bdfabea72e92d5d0
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-02-20 20:01:47 +0300

    Fixed detecting published packages

--- tools/publishedPackages.js
+++ tools/publishedPackages.js
@@ -1,7 +1,8 @@
 const exec = require("./exec");
 
 module.exports = async () => {
-  const { stdout } = await exec("git tag --points-at HEAD");
+  const { stdout: commit } = await exec("git rev-list --tags --max-count=1");
+  const { stdout } = await exec(`git tag --points-at ${commit}`);
 
   return stdout.split("\n").filter(l => l);
 };
