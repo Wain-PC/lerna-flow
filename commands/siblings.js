b12d4ffec2077390bec5a43195e590d0183df68a
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-04-03 19:17:17 +0300

    Fixed siblings behavior

--- commands/siblings.js
+++ commands/siblings.js
@@ -4,5 +4,7 @@ const update = require("../tools/updateSiblingRepositories");
 module.exports = async () => {
   if (await ask("Wanna update siblings?")) {
     await update();
+    return true;
   }
+  return false;
 };
