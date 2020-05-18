2fc4969151c503f897e1e86753a4462ef5d456b0
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-05-18 15:44:20 +0300

    Removed lerna directory check

--- tools/exec.js
+++ tools/exec.js
@@ -1,11 +1,8 @@
 const { command } = require("execa");
-const findProjectRoot = require("./findProjectRoot");
 
 module.exports = async (commandStr, opts) => {
-  const root = await findProjectRoot();
   const options = {
     preferLocal: true,
-    cwd: root,
     ...opts
   };
   return command(commandStr, options);
--- tools/spawn.js
+++ tools/spawn.js
@@ -1,13 +1,10 @@
 const { command } = require("execa");
 const logger = require("./logger");
-const findProjectRoot = require("./findProjectRoot");
 
 module.exports = async (commandString, opts) => {
-  const root = await findProjectRoot();
   const options = {
     stdio: "inherit",
     preferLocal: true,
-    cwd: root,
     ...opts
   };
 
